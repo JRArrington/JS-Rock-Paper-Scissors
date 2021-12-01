90bcae7413d83a610514b7374cc9e58c3f0f293a
Jamar Arrington <jamar.arrington.mad@gmail.com>
2021-12-01 21:56:34 +0000

    Completed humanPlay function Added playRound function (Rock choice only)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const options = ['Rock', 'Paper', 'Scissors'];
+const options = ['rock', 'paper', 'scissors'];
 
 function randomItem() {
   let random = options[Math.floor(Math.random()*options.length)];
@@ -12,4 +12,17 @@ function computerPlay() {
 
 }
 
-console.log(computerPlay());
+function humanPlay() {
+  let choice = prompt("Select: 'rock', 'paper', or 'scissors' ").toLowerCase();
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'rock') {
+      console.log(`Draw! You both played ${playerSelection}`);
+    }
+  }
+}
+
+playRound(humanPlay(), computerPlay());
