afbec27dc4c01bf356ef8f69a9a99ab8ba8f5027
Jamar Arrington <jamar.arrington.mad@gmail.com>
2021-12-02 01:54:35 +0000

    Implemented game function and increaseScore function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use strict';
 
+let rounds;
+let score = 0;
+
+function increaseScore() {
+  score += 1;
+  return score;
+}
+
 const options = ['rock', 'paper', 'scissors'];
 
 function randomItem() {
@@ -24,10 +32,12 @@ function playRound(playerSelection, computerSelection) {
     } else if (computerSelection === 'paper') {
       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     } else {
+      increaseScore();
       console.log(`You Win!!! ${playerSelection} beats ${computerSelection}`);
     }
   } else if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
+      increaseScore();
       console.log(`You Win!!! ${playerSelection} beats ${computerSelection}`);
     } else if (computerSelection === 'paper') {
       console.log(`Draw! You both played ${playerSelection}`);
@@ -38,6 +48,7 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'rock') {
       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     } else if (computerSelection === 'paper') {
+      increaseScore();
       console.log(`You Win!!! ${playerSelection} beats ${computerSelection}`);
     } else {
       console.log(`Draw! You both played ${playerSelection}`);
@@ -45,4 +56,11 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-playRound(humanPlay(), computerPlay());
+function game() {
+  for(let rounds = 1; rounds <= 5; rounds++) {
+    playRound(humanPlay(), computerPlay());
+  }
+  console.log(`You won ${score} rounds`);
+}
+
+game();
